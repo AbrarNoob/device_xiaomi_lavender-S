ArrowOS-Devices/android_device_xiaomi_lavender

lavender: vibrator: Add config to exclude haptics devices from input subsystem
Haptics devices which registers as input FF devices are not supposed to
be detected by input subsystem. Add excluded-input-devices.xml file for
input subsystem to exclude them.

Change-Id: I52dc6e502b29e2813299d073195a0991bb2cf692
Signed-off-by: Ratoriku <a1063021545@gmail.com>

device.mk
@@ -77,6 +77,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     LavenderWifiOverlay

AbrarNoob/device_xiaomi_lavender-S

Signed-off-by: ImPrashantt <prashant33968@gmail.com>

device.mk
@@ -442,6 +442,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \