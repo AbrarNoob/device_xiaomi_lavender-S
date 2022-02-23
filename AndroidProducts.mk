0888de469bf3c35e0a65c1c2d046126e014d337a
AbrarNoob <hasinabrarhaque@gmail.com>
2022-02-23 12:04:26 +0600

    init xd

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ancient_lavender.mk
+    $(LOCAL_DIR)/xdroid_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    ancient_lavender-user \
-    ancient_lavender-userdebug \
-    ancient_lavender-eng
+    xdroid_lavender-user \
+    xdroid_lavender-userdebug \
+    xdroid_lavender-eng
